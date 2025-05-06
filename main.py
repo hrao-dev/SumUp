3b05e21ec403951a3252dee0054c61d7b4c3e24f
Harini Rao <harini12@gmail.com>
2025-05-06 15:30:54 -0500

    updated model name

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from dotenv import load_dotenv
 app = FastAPI()
 
 # Initialize the summarization pipeline
-summarizer = pipeline("summarization",model="harao-ml/news-summary-finetuned")
+summarizer = pipeline("summarization",model="harao-ml/flant5-finetuned-summarize")
 
 class TextRequest(BaseModel):
     text: str
